e32e5899c962ea9ad673aae3f438a0abbd96d3fb
GAURAV-13079 <gauravsingla482@gmail.com>
2022-01-05 21:26:40 +0530

    added delete button

--- NodeJS/Contact List/index.js
+++ NodeJS/Contact List/index.js
@@ -39,6 +39,15 @@ app.post("/create-contact", function(req, res){
 })
 
 
+app.get("/delete-contact/:contact", function(req, res){
+    var para=req.params.contact;
+    var index=contactList.findIndex(contact=>contact.contact==para);
+
+    contactList.splice(index, 1);
+    return res.redirect("back");
+})
+
+
 app.listen(port, function(err){
     if (err){
         console.log(err);
