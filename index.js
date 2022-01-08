c87fdfda7b58df027fbdf0aaf3c2c0535df81998
GAURAV-13079 <gauravsingla482@gmail.com>
2022-01-08 12:50:59 +0530

    connected ejs with css

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const ejs=require("ejs");
 const app=express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static(__dirname + '/public'));
+
+var tasks=[{
+    name: "complete project",
+    date: "10 Jan 2022",
+    type: "work"
+}]
+
 
 app.get("/", function(req, res){
-    return res.render("home");
+    return res.render("home", {
+        'tasks': tasks
+    });
 })
 
 app.listen(port, function(err){
