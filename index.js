42ecf7460fb03b7c33afc887ea53969300e440e7
GAURAV-13079 <gauravsingla482@gmail.com>
2022-01-09 23:44:01 +0530

    completed all tasks without DB

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ const ejs=require("ejs");
 const app=express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
-var tasks=[{
-    name: "complete project",
-    date: "10 Jan 2022",
-    type: "work"
-}]
+var tasks=[];
 
 
 app.get("/", function(req, res){
     return res.render("home", {
         'tasks': tasks
     });
+})
+
+app.post("/add", function(req, res){
+    let data=req.body;
+    data["id"]=tasks.length;
+    if (data.name=="" || data.date=="" || data.type==""){
+        console.log("Please Fill all the data");
+        return;
+    }
+    
+    tasks.push(data);
+    return res.redirect("/");
+})
+
+app.get("/del/:id", function(req, res){
+    let delTask=tasks.filter(task=>task.id==req.params.id);
+    tasks.splice(tasks.indexOf(delTask[0]), 1);
+
+    return res.redirect("/");
 })
 
 app.listen(port, function(err){
